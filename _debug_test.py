686e943d082034a0a73cf35cabec358c9ffef022
Zoltan Csaszar <csaszar.zoltan@gmail.com>
2026-07-30 20:29:19 -0400

    chore: remove leftover debug files from anti-detection sprint

--- _debug_test.py
+++
@@ -1,6 +0,0 @@
-"""Debug test to verify the module being loaded."""
-import sys, os, inspect
-
-# Locate profile_manager
-from profile_manager import ProfileManager
-print(f"ProfileManager loaded from: {inspect.getfile(ProfileManager)}")
